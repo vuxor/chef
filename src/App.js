af12c902904f8dc1033dc3b81faffa284b7a63ff
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-12-02 15:33:04 +0100

    Ant modular import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +8,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Row>
+          <Col span={24}>
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to React!</h2>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </Col>
+        </Row>
       </div>
     );
   }
