406c463df16b07eae6572bd6f33519b64a050c84
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-12-06 23:04:30 +0100

    Dynamic form for ingredients

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'antd';
 
+import { WrappedDynamicFieldSet } from './Form';
 import './App.css';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
             <p className="App-intro">
               To get started, edit <code>src/App.js</code> and save to reload.
             </p>
+            <WrappedDynamicFieldSet />
           </Col>
         </Row>
       </div>
---
+++ src/Form.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Form, Input, Icon, Button } from 'antd';
+
+import './Form.css';
+const FormItem = Form.Item;
+
+let uuid = 0;
+class DynamicFieldSet extends React.Component {
+  componentWillMount() {
+    this.props.form.setFieldsValue({
+      keys: [0],
+    });
+  }
+
+  remove = (k) => {
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    // We need at least one passenger
+    if (keys.length === 1) {
+      return;
+    }
+
+    // can use data-binding to set
+    form.setFieldsValue({
+      keys: keys.filter(key => key !== k),
+    });
+  }
+
+  add = () => {
+    uuid++;
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    const nextKeys = keys.concat(uuid);
+    // can use data-binding to set
+    // important! notify form to detect changes
+    form.setFieldsValue({
+      keys: nextKeys,
+    });
+  }
+
+  render() {
+    const { getFieldDecorator, getFieldValue } = this.props.form;
+    const formItemLayout = {
+      labelCol: { span: 4 },
+      wrapperCol: { span: 20 },
+    };
+    const formItemLayoutWithOutLabel = {
+      wrapperCol: { span: 20, offset: 4 },
+    };
+
+    const keys = getFieldValue('keys');
+    const formItems = keys.map((k, index) => {
+      return (
+        <FormItem
+          {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+          label={index === 0 ? 'Ingredients' : ''}
+          required={false}
+          key={k}
+        >
+          {getFieldDecorator(`names-${k}`, {
+            validateTrigger: ['onChange', 'onBlur'],
+            rules: [{
+              required: true,
+              whitespace: true,
+              message: "Please input ingredient name or delete this field.",
+            }],
+          })(
+            <Input placeholder="ingredient name" style={{ width: '60%', marginRight: 8 }} />
+          )}
+          <Icon
+            className="dynamic-delete-button"
+            type="minus-circle-o"
+            disabled={keys.length === 1}
+            onClick={() => this.remove(k)}
+          />
+        </FormItem>
+      );
+    });
+    return (
+      <Form horizontal>
+        {formItems}
+        <FormItem {...formItemLayoutWithOutLabel}>
+          <Button type="dashed" onClick={this.add} style={{ width: '60%' }}>
+            <Icon type="plus" /> Add
+          </Button>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+export const WrappedDynamicFieldSet = Form.create()(DynamicFieldSet);
+
