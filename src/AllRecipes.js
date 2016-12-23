a1874078181020a401190d32844b3aa8112c6829
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-12-23 14:40:34 +0100

    Recipes method for AllRecipes component

--- src/AllRecipes.js
+++ src/AllRecipes.js
@@ -3,9 +3,22 @@ import { observer } from 'mobx-react';
 
 @observer
 export default class AllRecipes extends Component {
+  recipes() {
+    if (this.props.recipesStore.recipes.length) {
+      for (let recipe of this.props.recipesStore.recipes) {
+        console.log(recipe);
+      }
+    }
+    return 'no recipes for now';
+  }
   render() {
     return (
-      <div>All recipes</div>
+      <div>
+        <h2>All recipes</h2>
+        <div>
+          {this.recipes()}
+        </div>
+      </div>
     )
   }
 }
