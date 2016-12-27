8081336b527c4f77e335efdc2bb4e212d2b4a4e6
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-12-27 07:59:29 +0100

    Display recipes

--- src/AllRecipes.js
+++ src/AllRecipes.js
@@ -4,20 +4,52 @@ import { observer } from 'mobx-react';
 @observer
 export default class AllRecipes extends Component {
   recipes() {
+    let recipesDiv = (<div>no recipes for now</div>);
     if (this.props.recipesStore.recipes.length) {
+      console.log(this.props.recipesStore.recipes);
       for (let recipe of this.props.recipesStore.recipes) {
         console.log(recipe);
+        let ingArray = [];
+        for (let ing of recipe.ingredients) {
+          ingArray.push(ing);
+        }
+        recipesDiv = (
+          <div>
+            <p>
+              {recipe.name}
+            </p>
+            <ul>
+              {ingArray.map((ing, index) => (
+                <li key={index}>{ing}</li>
+              ))}
+            </ul>
+            <p>
+              {recipe.directions}
+            </p>
+          </div>
+        )
       }
     }
-    return 'no recipes for now';
+    return recipesDiv;
   }
   render() {
     return (
-      <div>
+      <div className="recipe">
         <h2>All recipes</h2>
-        <div>
-          {this.recipes()}
-        </div>
+        {this.props.recipesStore.recipes.length ?
+            this.props.recipesStore.recipes.map((recipe, index) => (
+              <div key={index}>
+                <p>{recipe.name}</p>
+                <ul>
+                  {recipe.ingredients.map((ing, i) => (
+                    <li key={i}>{ing}</li>
+                  ))}
+                </ul>
+                <p>
+                  {recipe.directions}
+                </p>
+              </div>
+            )): 'No recipes for now'}
       </div>
     )
   }
